77e4e8e2caa0eea7e11cb0da77e216410827ecd9
Naufal90 <45502745+Naufal90@users.noreply.github.com>
2026-09-13 12:40:40 +0700

    Add TurmusApplication class with crash handler

---
+++ app/src/main/java/com/offlinep2p/turmus/TurmusApplication.kt
@@ -0,0 +1,37 @@
+package com.offlinep2p.turmus
+
+import android.app.Application
+import com.offlinep2p.core.common.DebugLogger
+
+class TurmusApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        DebugLogger.info(
+            "APP",
+            "Application started",
+        )
+
+        installCrashHandler()
+    }
+
+    private fun installCrashHandler() {
+        val previousHandler =
+            Thread.getDefaultUncaughtExceptionHandler()
+
+        Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+
+            DebugLogger.error(
+                tag = "CRASH",
+                message = "Uncaught exception on thread=${thread.name}",
+                throwable = throwable,
+            )
+
+            previousHandler?.uncaughtException(
+                thread,
+                throwable,
+            )
+        }
+    }
+}
